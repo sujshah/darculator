32cfa5b3854f8c41a9715a8f9149900a8bd77f70
Suraj Shah <suraj1.shah@gmail.com>
2022-05-02 15:04:27 +0100

    Add functionality for working calculator

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-let DISPLAY_VALUE = "";
-let FIRST_VALUE;
-let SECOND_VALUE;
-let OPERATOR;
-let OPERATOR_BY_SYMBOL = {
+let DISPLAY_VALUE = "0";
+let CURRENT_NUMBER;
+let OPERATOR_SYMBOL;
+let SET_FLOATING_POINT = false;
+const PRECISION = 1e15;
+const OPERATOR_BY_SYMBOL = {
     "+": add,
     "-": subtract,
-    "x": multiply,
+    "*": multiply,
     "/": divide,
 }
+let INFINITY_ANSWER = "lmao";
 
 function add(a, b) {
     return a + b;
@@ -22,9 +24,6 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        return undefined;
-    }
     return a / b;
 }
 
@@ -37,30 +36,63 @@ function outputDisplayValue() {
     element.textContent = DISPLAY_VALUE.toString();
 }
 
-function getMouseClick() {
+function processMouseClick() {
     document.querySelectorAll('.button').forEach(
         element => element.addEventListener('click', populateDisplay)
     );
+    document.querySelector('.clear').addEventListener('click', clearDisplay)
+}
+
+function computeAnswer() {
+    if (OPERATOR_SYMBOL === undefined) return DISPLAY_VALUE;
+    const secondNumber = DISPLAY_VALUE.split(OPERATOR_SYMBOL)[1];
+    if (secondNumber === "") return DISPLAY_VALUE;
+    if (secondNumber === "0" && OPERATOR_SYMBOL === "/") {
+        return "lmao";
+    }
+    const operator = OPERATOR_BY_SYMBOL[OPERATOR_SYMBOL]
+    return Math.round(operate(operator, CURRENT_NUMBER, parseFloat(secondNumber))*PRECISION) / PRECISION;
+
 }
 
 function populateDisplay(event) {
-    let key = event.target.textContent;
-    if (!key) return;
-    if (["+", "-", "/", "x"].includes(key)) {
-        console.log(key);
-        FIRST_VALUE = parseInt(DISPLAY_VALUE);
-        OPERATOR = key;
-        DISPLAY_VALUE += OPERATOR;
+    let value = event.target.textContent;
+    if (DISPLAY_VALUE === INFINITY_ANSWER) {
+        return;
     }
-    else if (key === "=") {
-        SECOND_VALUE = parseInt(DISPLAY_VALUE.split(OPERATOR)[1]);
-        DISPLAY_VALUE = OPERATOR_BY_SYMBOL[OPERATOR](FIRST_VALUE, SECOND_VALUE);
+    if (Object.keys(OPERATOR_BY_SYMBOL).includes(value)) {
+        if (!(OPERATOR_SYMBOL === undefined)) {
+            DISPLAY_VALUE = computeAnswer();
+        }
+        CURRENT_NUMBER = parseFloat(DISPLAY_VALUE);
+        OPERATOR_SYMBOL = value;
+        DISPLAY_VALUE += OPERATOR_SYMBOL;
+        resetFloatingPoint();
+    }
+    else if (value === "=") {
+        DISPLAY_VALUE = computeAnswer();
+        OPERATOR_SYMBOL = undefined;
+        resetFloatingPoint();
+    }
+    else if (value === ".") {
+        if (!SET_FLOATING_POINT) DISPLAY_VALUE += value;
+        SET_FLOATING_POINT = true;
     }
     else {
-        DISPLAY_VALUE += key;
+        DISPLAY_VALUE = (DISPLAY_VALUE === "0") ? value: DISPLAY_VALUE + value;
     }
     outputDisplayValue();
 }
 
+function resetFloatingPoint() {
+    SET_FLOATING_POINT = false;
+}
 
-getMouseClick();
+function clearDisplay() {
+    DISPLAY_VALUE = "0";
+    CURRENT_NUMBER = OPERATOR_SYMBOL = undefined;
+    resetFloatingPoint();
+    outputDisplayValue();
+}
+
+processMouseClick();
