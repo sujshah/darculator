c8e1b72785dcfb7c540c9090476570c7aecb8d66
Suraj Shah <suraj1.shah@gmail.com>
2022-05-02 10:46:57 +0100

    Add functionality to populate display

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const add = function(a, b) {
+let DISPLAY_VALUE = "";
+
+function add(a, b) {
     return a + b;
 }
 
@@ -20,3 +22,23 @@ function divide(a, b) {
 function operate(operator, a, b) {
     return operator(a, b);
 }
+
+function outputDisplayValue() {
+    let element = document.querySelector('.display');
+    element.textContent = DISPLAY_VALUE.toString();
+}
+
+function getMouseClick() {
+    document.querySelectorAll('.button').forEach(
+        element => element.addEventListener('click', populateDisplay)
+    )
+}
+
+function populateDisplay(event) {
+    let key = event.target.textContent;
+    if (!key) return;
+    DISPLAY_VALUE += key;
+    outputDisplayValue();
+}
+
+getMouseClick();
